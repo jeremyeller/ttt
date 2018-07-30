99211bdb72348e7693415dd87ccb8d42d65fa2b2
Jeremy Eller <jeremy@campusesp.com>
2018-07-30 10:12:50 -0400

    Added a tile image and some other things

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'tic_tac_toe#play'
-  get 'tic_tac_toe/play'
-  get 'tic_tac_toe/game_over'
+  get '/play', to: 'tic_tac_toe#play'
+  get '/game_over', to: 'tic_tac_toe#game_over'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
